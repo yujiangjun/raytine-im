cad9dc5d02c3e0da3f107b2c719a9571c99f4b4c
yujiangjun <18512106361>
2022-10-21 23:44:36 +0800

    环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import path from "node:path";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
